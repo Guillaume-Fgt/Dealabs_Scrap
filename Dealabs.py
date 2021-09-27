d470b3985d7b282c08836b176a6c0e9153ae55c4
Your Name <you@example.com>
2021-09-27 09:11:26 +0200

    Virtual environment

--- Dealabs.py
+++ Dealabs.py
@@ -1,4 +1,3 @@
-import time
 from collections import Counter
 from operator import itemgetter
 
@@ -10,18 +9,21 @@ from bs4 import BeautifulSoup
 def scrap_page(page, list):
     soup = BeautifulSoup(page.content, "lxml")  # lxml faster thant html.parser
     # bleeze 77 hot deal page (infinite scrolling)
-    bleeze_soup = soup("span", "cept-merchant-name text--b text--color-greyShade link")
+    bleeze_soup = soup("span",
+                       "cept-merchant-name text--b text--color-greyShade link")
     for merchant in bleeze_soup:
         list += [
             merchant.contents[0]
-        ]  # .text doesn't work, don't know why. I use contents, it returns a list so [0]
+        ]
 
 
 merchants = []
 for i in range(1, 13):
     url = f"https://www.dealabs.com/profile/Bleezes77/bons-plans?page={i}"
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
+        "User-Agent": ("Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+                       "AppleWebKit/537.36 (KHTML, like Gecko)"
+                       "Chrome/83.0.4103.116 Safari/537.36")
     }
     page = requests.get(url, headers=headers)
     scrap_page(page, merchants)
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Dealabs_Scrap
-Scrapper les deals d'un utilisateur pour avoir la liste des sites par ordre d'utilisation. Je recherche un ordinateur et je voulais étudier les sites qui revenaient le plus souvent chez un dealer proposant souvent de bons plans en montage pc.
+Scrapper les deals d'un utilisateur pour avoir la liste des sites par ordre d'utilisation
 
 Aller sur la page deals d'un utilisateur et récupérer l'adresse http:
 https://www.dealabs.com/profile/Bleezes77/bons-plans
@@ -12,11 +12,4 @@ Modifier l'url ligne 22 pour mettre le nom d'utilisateur souhaité.
 
 Exécuter le script.
 Résultat:
-![Alt text](image.png "Title")
-
-Limitations: prend en compte uniquement les deals expirés.
-
-TODOLIST:
-- détecter automatiquement le nombre de pages de deal. 
-- uniquement avoir à entrer le nom d'utilisateur serait bien.
-- meilleure mise en forme dans matplotlib
+(image.png)
